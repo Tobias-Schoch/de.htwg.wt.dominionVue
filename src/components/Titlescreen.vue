6a8402afa16bc1bdc2e1c894e2a63a3d1b591b52
tobias-schoch <tobias@schochwebdesign.de>
2021-01-06 16:47:41 +0100

    Working on redirecting

--- src/components/Titlescreen.vue
+++ src/components/Titlescreen.vue
@@ -14,17 +14,31 @@
         <v-row>
             <v-col align="center"
                    justify="center">
-                <router-link to="/about">asdasdTasdest</router-link>
-                <button class="btn">
-                    Play
+                <button class="btn play">
+
+                    <v-img class="button-icon"
+                           height="30px"
+                           width="30px"
+                           src="img/arrow.png">
+                    </v-img>
+                    <div class="text">
+                        Play
+                    </div>
                 </button>
             </v-col>
         </v-row>
         <v-row>
             <v-col align="center"
                    justify="center">
-                <button class="btn">
-                    Rules
+                <button class="btn rules">
+                    <v-img class="button-icon"
+                           height="30px"
+                           width="30px"
+                           src="img/arrow.png">
+                    </v-img>
+                    <div class="text">
+                        Rules
+                    </div>
                 </button>
             </v-col>
         </v-row>
@@ -38,7 +52,28 @@
 </script>
 
 <style>
+
+    .play .v-image, .rules .v-image {
+        display: none;
+    }
+
+    .play:hover .v-image, .rules:hover .v-image {
+        display: block;
+    }
+
+    .v-image.button-icon {
+        float: left;
+        margin-right: 15px;
+        margin-top: 0 !important;
+    }
+
+    .text {
+        display: block;
+        margin-left: 15px;
+    }
+
     .btn {
+        text-align: center;
         text-transform: uppercase;
         font-size: 25px;
         font-family: 'McLaren', cursive;
@@ -48,11 +83,6 @@
         -webkit-border-radius: 70px;
         border-radius: 70px;
         outline: none;
-        background-color: #eee;
-    }
-
-    .btn:hover {
-        background-color: #9ec4e8;
     }
 
     .btn, .btn:hover {
