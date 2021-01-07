1d5aaf2dc6ebfd91d0e79091c1f1925b6bcfd6ef
tobias-schoch <tobias@schochwebdesign.de>
2021-01-07 03:06:10 +0100

    Images now displayed

--- src/components/GameHandCards.vue
+++ src/components/GameHandCards.vue
@@ -17,10 +17,10 @@
         var cards = "";
         var timerID = setInterval(function () {
             cards = $(".hidden-win-hand").html();
-            if (cards.indexOf("cardName") >= 0) {
+            if (cards.includes("cardName")) {
                 cards = JSON.parse(cards);
                 for (var i = 0; i < cards[0].length; i++) {
-                    $('.playing-decks-hand').append('<v-col id="card_' + i + '" class="card" @click="push_hand(' + i + ')"><img class="card_image" src="img/cards/' + cards[0][i].cardName + '.png"></v-col>');
+                    $('.playing-decks-hand').append('<div id="card_' + i + '" class="card" @click.native="push_hand(' + i + ')"><img class="card_image" src="img/cards/' + cards[0][i].cardName + '.png"></div>');
                 }
                 clearInterval(timerID)
             }
--- src/components/GamePlayingCards.vue
+++ src/components/GamePlayingCards.vue
@@ -5,26 +5,29 @@
         <v-row class="playing-decks"
                align="center"
                justify="center"></v-row>
+
     </div>
 </template>
 
 <script>
-    import {mapGetters} from "vuex";
+
     import jQuery from "jquery";
 
     jQuery(document).ready(function ($) {
         var cards = "";
         var timerID = setInterval(function () {
             cards = $(".hidden-win-playing").html();
-            if (cards.indexOf("Copper") >= 0) {
+            if (cards.includes("Copper")) {
                 cards = JSON.parse(cards);
                 for (var i = 0; i < cards[0].length; i++) {
-                    $('.playing-decks').append('<v-col id="card_' + i + '" class="card" @click="push_play(' + i + ')"><img class="card_image" src="img/cards/' + cards[0][i][0].cardName + '.png"></v-col>');
+                    $('.playing-decks').append('<div id="card_' + i + '" class="card" @click.native="push_play(' + i + ')"><img class="card_image" src="img/cards/' + cards[0][i][0].cardName + '.png"></div>');
                 }
                 clearInterval(timerID)
             }
         }, 50);
     });
+
+    import {mapGetters} from "vuex";
 
     export default {
         name: "GamePlayingCards",
@@ -38,7 +41,7 @@
                 this.$store.dispatch("request", cardId);
             },
             push_play(i) {
-                console.log("i send" + i);
+                alert("i send" + i);
                 this.$store.dispatch("request", i);
             }
         }
