7798eb3532b0a9f2aac8eec27dade7524865405b
tobias-schoch <tobias@schochwebdesign.de>
2021-01-10 21:22:58 +0100

    Finished PWA

--- dominion-pwa/vue.config.js
+++ dominion-pwa/vue.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   pwa: {
     name: 'Dominion PWA',
-    themeColor: '#323232',
-    msTileColor: '#5f9ea0',
+    lang: 'de-DE',
+    orientation: 'portrait',
+    description: 'Dominion is a card board game for 3 to 5 players.',
+    background_color: 'white',
+    themeColor: '#948d29',
+    msTileColor: '#948d29',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
+    appleMobileWebAppStatusBarStyle: 'white',
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
       skipWaiting: true,
@@ -15,7 +19,7 @@ module.exports = {
       name: 'Dominion PWA',
       short_name: 'Dominion',
       start_url: '.',
-      display: 'fullscreen',
+      display: 'standalone',
     }
   },
   "transpileDependencies": [
